a4040892e58de47b886b01c7ff1b75b4a7bb6e5e
vvaucoul <vvaucoul@student.42.fr>
2024-02-11 20:57:39 +0100

    [Update] - Printk / Stdio lib

--- Hephaistos/libs/stdio/printk/srcs/printk.c
+++ Hephaistos/libs/stdio/printk/srcs/printk.c
@@ -6,7 +6,7 @@
 /*   By: vvaucoul <vvaucoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 15:06:11 by vvaucoul          #+#    #+#             */
-/*   Updated: 2024/02/11 12:47:11 by vvaucoul         ###   ########.fr       */
+/*   Updated: 2024/02/11 20:57:23 by vvaucoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ static int check_special_strings(const char *str) {
 }
 
 static int check_colors(const char *str) {
+    /* Check Foreground colors */
     if (strncmp(str, _END, strlen(_END)) == 0) {
         terminal_setcolor(VGA_COLOR_LIGHT_GREY);
         return (strlen(_END));
@@ -45,7 +46,81 @@ static int check_colors(const char *str) {
     } else if (strncmp(str, _CYAN, strlen(_CYAN)) == 0) {
         terminal_setcolor(VGA_COLOR_CYAN);
         return (strlen(_CYAN));
+    } else if (strncmp(str, _LGREY, strlen(_LGREY)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_GREY);
+        return (strlen(_LGREY));
+    } else if (strncmp(str, _LBLUE, strlen(_LBLUE)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_BLUE);
+        return (strlen(_LBLUE));
+    } else if (strncmp(str, _LGREEN, strlen(_LGREEN)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_GREEN);
+        return (strlen(_LGREEN));
+    } else if (strncmp(str, _LCYAN, strlen(_LCYAN)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_CYAN);
+        return (strlen(_LCYAN));
+    } else if (strncmp(str, _LRED, strlen(_LRED)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_RED);
+        return (strlen(_LRED));
+    } else if (strncmp(str, _LMAGENTA, strlen(_LMAGENTA)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_MAGENTA);
+        return (strlen(_LMAGENTA));
+    } else if (strncmp(str, _LYELLOW, strlen(_LYELLOW)) == 0) {
+        terminal_setcolor(VGA_COLOR_LIGHT_BROWN);
+        return (strlen(_LYELLOW));
+    } else if (strncmp(str, _WHITE, strlen(_WHITE)) == 0) {
+        terminal_setcolor(VGA_COLOR_WHITE);
+        return (strlen(_WHITE));
     }
+
+    /* Check Background colors */
+    if (strncmp(str, _BG_BLACK, strlen(_BG_BLACK)) == 0) {
+        terminal_set_background_color(VGA_COLOR_BLACK);
+        return (strlen(_BG_BLACK));
+    } else if (strncmp(str, _BG_BLUE, strlen(_BG_BLUE)) == 0) {
+        terminal_set_background_color(VGA_COLOR_BLUE);
+        return (strlen(_BG_BLUE));
+    } else if (strncmp(str, _BG_GREEN, strlen(_BG_GREEN)) == 0) {
+        kpause();
+        terminal_set_background_color(VGA_COLOR_GREEN);
+        return (strlen(_BG_GREEN));
+    } else if (strncmp(str, _BG_CYAN, strlen(_BG_CYAN)) == 0) {
+        terminal_set_background_color(VGA_COLOR_CYAN);
+        return (strlen(_BG_CYAN));
+    } else if (strncmp(str, _BG_RED, strlen(_BG_RED)) == 0) {
+        terminal_set_background_color(VGA_COLOR_RED);
+        return (strlen(_BG_RED));
+    } else if (strncmp(str, _BG_MAGENTA, strlen(_BG_MAGENTA)) == 0) {
+        terminal_set_background_color(VGA_COLOR_MAGENTA);
+        return (strlen(_BG_MAGENTA));
+    } else if (strncmp(str, _BG_YELLOW, strlen(_BG_YELLOW)) == 0) {
+        terminal_set_background_color(VGA_COLOR_BROWN);
+        return (strlen(_BG_YELLOW));
+    } else if (strncmp(str, _BG_LGREY, strlen(_BG_LGREY)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_GREY);
+        return (strlen(_BG_LGREY));
+    }  else if (strncmp(str, _BG_LBLUE, strlen(_BG_LBLUE)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_BLUE);
+        return (strlen(_BG_LBLUE));
+    } else if (strncmp(str, _BG_LGREEN, strlen(_BG_LGREEN)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_GREEN);
+        return (strlen(_BG_LGREEN));
+    } else if (strncmp(str, _BG_LCYAN, strlen(_BG_LCYAN)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_CYAN);
+        return (strlen(_BG_LCYAN));
+    } else if (strncmp(str, _BG_LRED, strlen(_BG_LRED)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_RED);
+        return (strlen(_BG_LRED));
+    } else if (strncmp(str, _BG_LMAGENTA, strlen(_BG_LMAGENTA)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_MAGENTA);
+        return (strlen(_BG_LMAGENTA));
+    } else if (strncmp(str, _BG_LYELLOW, strlen(_BG_LYELLOW)) == 0) {
+        terminal_set_background_color(VGA_COLOR_LIGHT_BROWN);
+        return (strlen(_BG_LYELLOW));
+    } else if (strncmp(str, _BG_WHITE, strlen(_BG_WHITE)) == 0) {
+        terminal_set_background_color(VGA_COLOR_WHITE);
+        return (strlen(_BG_WHITE));
+    }
+
     return (0);
 }
 
--- Hephaistos/libs/stdio/stdio.h
+++ Hephaistos/libs/stdio/stdio.h
@@ -6,7 +6,7 @@
 /*   By: vvaucoul <vvaucoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 13:13:31 by vvaucoul          #+#    #+#             */
-/*   Updated: 2024/02/11 12:28:34 by vvaucoul         ###   ########.fr       */
+/*   Updated: 2024/02/11 20:56:58 by vvaucoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,42 @@
  ******************************************************************************/
 
 #define _BLACK "\033[0;30m"
-#define _BLUE "\033[0;31m"
+#define _RED "\033[0;31m"
 #define _GREEN "\033[0;32m"
-#define _CYAN "\033[0;33m"
-#define _RED "\033[0;34m"
+#define _YELLOW "\033[0;33m"
+#define _BLUE "\033[0;34m"
 #define _MAGENTA "\033[0;35m"
-#define _YELLOW "\033[0;36m"
-#define _LGREY "\033[0;37m"
-#define _DGREY "\033[0;38m"
-#define _LBLUE "\033[0;39m"
-#define _LGREEN "\033[0;40m"
-#define _LCYAN "\033[0;41m"
-#define _LRED "\033[0;42m"
-#define _LMAGENTA "\033[0;43m"
-#define _LYELLOW "\033[0;44m"
-#define _WHITE "\033[0;45m"
+#define _CYAN "\033[0;36m"
+#define _WHITE "\033[0;37m"
+
+#define _LGREY "\033[0;90m"
+#define _LRED "\033[0;91m"
+#define _LGREEN "\033[0;92m"
+#define _LYELLOW "\033[0;93m"
+#define _LBLUE "\033[0;94m"
+#define _LMAGENTA "\033[0;95m"
+#define _LCYAN "\033[0;96m"
+#define _LWHITE "\033[0;97m"
+
 #define _END "\033[0m"
+
+#define _BG_BLACK "\033[0;40m"
+#define _BG_RED "\033[0;41m"
+#define _BG_GREEN "\033[0;42m"
+#define _BG_YELLOW "\033[0;43m"
+#define _BG_BLUE "\033[0;44m"
+#define _BG_MAGENTA "\033[0;45m"
+#define _BG_CYAN "\033[0;46m"
+#define _BG_WHITE "\033[0;47m"
+
+#define _BG_LGREY "\033[0;100m"
+#define _BG_LRED "\033[0;101m"
+#define _BG_LGREEN "\033[0;102m"
+#define _BG_LYELLOW "\033[0;103m"
+#define _BG_LBLUE "\033[0;104m"
+#define _BG_LMAGENTA "\033[0;105m"
+#define _BG_LCYAN "\033[0;106m"
+#define _BG_LWHITE "\033[0;107m"
 
 #define _BOLD "\033[1m"
 #define _DIM "\033[2m"
